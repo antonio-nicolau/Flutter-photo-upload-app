4288303a07e6ffbcadd34e6e8842758c9dfbec22
antonionicolau <antonio.tioypedro1234@gmail.com>
2023-08-23 20:12:31 +0100

    feat: added camera support for new-image page

--- lib/src/modules/new_image/new_image_page.dart
+++ lib/src/modules/new_image/new_image_page.dart
@@ -1,35 +1,31 @@
+import 'dart:io';
+import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:better_open_file/better_open_file.dart';
 
-class NewImagePage extends StatefulWidget {
+class NewImagePage extends StatelessWidget {
   const NewImagePage({super.key});
 
-  @override
-  State<NewImagePage> createState() => _NewImagePageState();
-}
-
-class _NewImagePageState extends State<NewImagePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Click the button to add a new image',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 24),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.add),
-            ),
-          ],
+      body: CameraAwesomeBuilder.awesome(
+        saveConfig: SaveConfig.photo(
+          pathBuilder: () async {
+            final Directory extDir = await getTemporaryDirectory();
+            final testDir = await Directory(
+              '${extDir.path}/camerawesome.jpg',
+            ).create(recursive: true);
+            return testDir.path;
+          },
         ),
+        onMediaTap: (mediaCapture) {
+          OpenFile.open(mediaCapture.filePath);
+        },
       ),
     );
   }
